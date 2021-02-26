35ce338260dde10905a5a4ee696f37272dbdaa4d
Igor Emerick <igoremerickbarbosa@hotmail.com>
2021-02-26 10:45:04 -0300

    Iniciando os packages

---
+++ src/main/java/phobosxd/list/ExampleList.java
@@ -0,0 +1,18 @@
+package phobosxd.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExampleList {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Carlos");
+        nomes.add("Igor");
+        nomes.add("Iasmin");
+        nomes.add("Maria");
+        nomes.add("Joao");
+
+        System.out.println(nomes);
+    }
+}
